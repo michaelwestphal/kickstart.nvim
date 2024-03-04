1a015db1426c7949fbf8f20fef1832864219d58f
Michael Westphal <michael.westphal@nelnet.net>
2024-03-04 15:50:19 -0600

    Use relative line numbers and enable custom plugins
    Add file tree and git plugin

---
+++ lua/custom/plugins/git.lua
@@ -0,0 +1,10 @@
+-- https://github.com/NeogitOrg/neogit
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- Diff integration
+    'nvim-telescope/telescope.nvim', -- optional
+  },
+  config = true,
+}
