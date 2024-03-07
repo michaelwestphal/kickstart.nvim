74349e77627d5e4b1ddbb41836ed8e37ab8ff984
Michael Westphal <michael.westphal@nelnet.net>
2024-03-07 12:20:07 -0600

    Add comment with link to plugin GitHub project

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,3 +1,5 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
