88828f73b42de54b362a4f4bbdc35945b42f2347
Michael Westphal <michaelwestphal@hey.com>
2024-06-03 18:45:24 -0500

    Re-enable custom plugins
    And ensure additional language servers are installed.

--- init.lua
+++ init.lua
@@ -846,7 +846,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'javascript', 'ruby', 'elixir', 'angular'  },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'javascript', 'ruby', 'elixir', 'angular' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -896,7 +896,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
